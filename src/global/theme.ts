4a285ada5f25cde2d118045c477ab3635925ad47
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-03 12:34:59 -0300

    feat: configuring and enabling styled-components

---
+++ src/global/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colors: {
+    red: "#F74044",
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "../global/theme";
+import { GlobalStyle } from "../styles/Global";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
