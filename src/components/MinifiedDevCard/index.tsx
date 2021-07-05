6e0e3972f725b386cee9e16c8fb4ccc2796cb41b
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-04 23:54:53 -0300

    refactor: changing MinifiedDevCard component styles and comportment

--- src/components/MinifiedDevCard/index.tsx
+++ src/components/MinifiedDevCard/index.tsx
@@ -1,21 +1,37 @@
-import { FiUserCheck } from 'react-icons/fi';
-import { StyledMinifiedDevCard } from './styles';
+import Link from "next/link";
+import { FiUserCheck } from "react-icons/fi";
+import { StyledMinifiedDevCard } from "./styles";
 
 type MinifiedDevCardProps = {
-  login: string,
-  html_url: string,
-  avatar_url: string,
-  verified_user?: boolean,
-}
+  login: string;
+  html_url: string;
+  avatar_url: string;
+  verified_user?: boolean;
+};
 
-export function MinifiedDevCard({ login, avatar_url, html_url, verified_user = false }: MinifiedDevCardProps) {
+export function MinifiedDevCard({
+  login,
+  avatar_url,
+  verified_user = true,
+}: MinifiedDevCardProps) {
   return (
-    <StyledMinifiedDevCard>
-      <img src={avatar_url} alt={login} />
-      <div>
-        <a href={html_url}>@{login}</a>
-        {verified_user && <span><FiUserCheck /> Signed User</span>}
-      </div>
-    </StyledMinifiedDevCard>
-  )
+    <article>
+      <Link href={`/dev/${login}`} passHref={true}>
+        <StyledMinifiedDevCard>
+          <header>
+            <img src={avatar_url} alt={login} />
+          </header>
+
+          <strong>@{login}</strong>
+
+          {verified_user && (
+            <footer>
+              <FiUserCheck />
+              <span>Signed User</span>
+            </footer>
+          )}
+        </StyledMinifiedDevCard>
+      </Link>
+    </article>
+  );
 }
--- src/components/MinifiedDevCard/styles.tsx
+++ src/components/MinifiedDevCard/styles.tsx
@@ -1,43 +1,63 @@
 import styled from "styled-components";
 
-export const StyledMinifiedDevCard = styled.header`
+export const StyledMinifiedDevCard = styled.a`
   width: 100%;
   height: 100%;
-  max-width: 15.625rem;
-  padding: 2.5rem 0px;
+  max-width: 14.125rem;
+  padding: 2.5rem 1.5rem;
+  margin: 0 auto;
 
   background: ${(props) => props.theme.colors.white};
-  border-radius: .5rem;
-  box-shadow: 0px 24px 38px rgba(0, 0, 0, 0.07), 0px 9px 46px rgba(0, 0, 0, 0.05), 0px 11px 15px rgba(0, 0, 0, 0.13);
+  border-radius: 0.5rem;
+  box-shadow: 2px 4px 24px rgba(0, 0, 0, 0.08);
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 0.625rem;
 
-  & > img {
+  transition: box-shadow 0.3s, transform 0.3s;
+
+  &:hover {
+    transform: scale(1.01);
+    box-shadow: 0px 24px 38px rgba(0, 0, 0, 0.04),
+      0px 9px 46px rgba(0, 0, 0, 0.02), 0px 11px 15px rgba(0, 0, 0, 0.09);
+    filter: brightness(1);
+  }
+
+  header {
+    margin-bottom: 1rem;
+  }
+
+  header > img {
     width: 4.375rem;
     border-radius: 50%;
   }
 
-  & > div {
-    display: flex;
-    flex-direction: column;
+  strong {
+    font: ${(props) => props.theme.fonts.heading500};
+    color: ${(props) => props.theme.colors.subtitle};
+    margin-bottom: 1rem;
+  }
 
-    a {
-      font-size: 1.125rem;
-      color: ${(props) => props.theme.colors.subtitle};
+  footer {
+    display: flex;
+    align-items: center;
+    color: ${(props) => props.theme.colors.primary300};
+    gap: 0.5rem;
+
+    svg {
+      font-size: 1.5rem;
     }
 
     span {
-      display: flex;
-      align-items: center;
-      gap: 0.3125rem;
-      color: ${(props) => props.theme.colors.primary300};
-
-      svg {
-        font-size: 1.5rem;
-      }
+      font-size: 0.875rem;
     }
+  }
+
+  @media (max-width: 574px) {
+    width: 90vw;
+    max-width: 80vw;
+    padding: 1.5rem 1rem;
   }
 `;
