bd452f979f0f3317fb6ee89e19ffd2f73fb4c723
ryansldev <ryanlima1309@gmail.com>
2021-07-05 15:26:19 -0300

    feat: creating the Dev page

---
+++ src/pages/dev/[login].tsx
@@ -0,0 +1,126 @@
+import { useState, useEffect } from 'react';
+import { FiMapPin, FiLink, FiGithub, FiTwitter, FiMail } from 'react-icons/fi';
+import { useRouter } from 'next/router';
+
+import { Header } from '../../components/Header';
+import { MinifiedDevCard } from '../../components/MinifiedDevCard';
+
+import { api as githubAPI } from '../../services/github';
+
+import { Main } from '../../styles/pages/Dev';
+
+type DevQueryParams = {
+  login?: string,
+}
+
+type Dev = {
+  id?: string;
+  name?: string;
+  login?: string;
+  html_url?: string;
+  avatar_url?: string;
+  bio?: string,
+  location?: string;
+  blog?: string;
+  email?: string;
+  twitter_username?: string;
+}
+
+type Following = {
+  id: string;
+  login: string;
+  html_url?: string;
+  avatar_url?: string;
+}
+
+export default function Dev() {
+  const router = useRouter();
+  const { login }: DevQueryParams = router.query;
+  const [dev, setDev] = useState<Dev>({});
+  const [following, setFollowing] = useState<Following[]>([]);
+
+  useEffect(() => {
+    if (!login) {
+      return;
+    }
+
+    async function devData() {
+      const { data: devData } = await githubAPI.get(`/users/${login}`);
+      if(!devData.blog.includes('https://')) {
+        devData.blog = (`https://${devData.blog}`);
+      };
+      const { data: followingData } = await githubAPI.get(`/users/${login}/following`);
+      setDev(devData);
+      setFollowing(followingData);
+    }
+
+    devData();
+  }, [login]);
+
+  return (
+    <>
+      <Header />
+      <Main>
+        <div className="container">
+          <img src={dev.avatar_url} alt={dev.name} />
+          <h1>{dev.name}</h1>
+          <h2>@{login}</h2>
+          <p>{dev.bio}</p>
+          <div className="more-infos">
+            {dev.location &&
+              <div>
+                <FiMapPin />
+                <span>{dev.location}</span>
+              </div>
+            }
+            {dev.html_url &&
+              <div>
+                <FiGithub />
+                <a href={dev.html_url} target="_blank" rel="noopener noreferrer">
+                  {dev.html_url}
+                </a>
+              </div>
+            }
+            {dev.blog &&
+              <div>
+                <FiLink />
+                <a href={dev.blog} target="_blank" rel="noopener noreferrer">
+                  {dev.blog}
+                </a>
+              </div>
+            }
+            {dev.email &&
+              <div>
+                <FiMail />
+                <span>{dev.email}</span>
+              </div>
+            }
+            {dev.twitter_username &&
+              <div>
+                <FiTwitter />
+                <a href={`https://twitter.com/${dev.twitter_username}`} target="_blank" rel="noopener noreferrer">
+                  @{dev.twitter_username}
+                </a>
+              </div>
+            }
+          </div>
+          <section className="following">
+            {following !== [] ? <span>Quem <strong>{dev.name}</strong> segue?</span> : <span><strong>{dev.name}</strong> ainda não segue ninguém</span>}
+            <div>
+              {following.map((dev) => {
+                return (
+                  <MinifiedDevCard
+                    key={dev.id}
+                    login={dev.login}
+                    avatar_url={dev.avatar_url}
+                    html_url={dev.html_url}
+                  />
+                );
+              })}
+            </div>
+          </section>
+        </div>
+      </Main>
+    </>
+  )
+}
---
+++ src/styles/pages/Dev.tsx
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  margin-top: 3.75rem;
+  text-align: center;
+
+  > .container {
+    width: 100%;
+    max-width: 90vw;
+    margin: 0 auto;
+
+    > img {
+      border-radius: 50%;
+      width: 100px;
+      height: 100px;
+      border: 4px solid ${(props) => props.theme.colors.primary};
+      margin-bottom: 1rem;
+    }
+
+    > h1 {
+      color: ${(props) => props.theme.colors.primary};
+    }
+
+    > h2 {
+      font: ${(props) => props.theme.fonts.subtitle};
+      color: ${(props) => props.theme.colors.primary400};
+      margin-bottom: 1.875rem;
+    }
+
+    > p {
+      max-width: 20rem;
+      margin: 0 auto;
+    }
+
+    > .more-infos {
+      display: flex;
+      flex-direction: column;
+
+      width: 100%;
+      max-width: 17rem;
+      margin: 0 auto;
+      margin-top: 2.5rem;
+
+      gap: 1rem;
+      color: ${(props) => props.theme.colors.primary300};
+
+      > div  {
+        display: flex;
+        align-items: center;
+        gap: 1.125rem;
+
+        svg {
+          flex: 1;
+          max-width: 1.5rem;
+          font-size: 1.5rem;
+        }
+
+        > a, span {
+          flex: 1;
+          text-align: left;
+        }
+
+        > a {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+
+          &:hover {
+            color: ${(props) => props.theme.colors.primary300};
+            filter: none;
+          }
+        }
+      }
+    }
+
+    > section {
+      margin-top: 7.5rem;
+      color: ${(props) => props.theme.colors.primary400};
+
+      > span {
+        font: ${(props) => props.theme.fonts.heading400};
+        font-size: 2rem;
+
+        strong {
+          font-size: 2rem;
+        }
+      }
+
+      > div {
+        max-width: 90vw;
+        width: 73.125rem;
+
+        margin: 3.75rem 0;
+
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(16.125rem, auto));
+        justify-content: center;
+        gap: 1.5rem 0;
+
+        @media (max-width: 572px) {
+          display: flex;
+          flex-direction: column;
+        }
+      }
+    }
+  }
+`
