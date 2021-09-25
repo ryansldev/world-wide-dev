bd0fbccef415553ca047320babdc4cdecf91dfd4
unknown <ryanlima1309@gmail.com>
2021-09-25 12:18:35 -0300

    refactor: adjustments on the code

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -456,9 +456,11 @@ export default function Home({ usersIds }: dashboardProps) {
         { recommendedDevs.length === 0 && user && <h2>Make your recommended search of devs!</h2> }
         { recommendedDevs.length === 0 && !user && <h2>SignIn and search for recommended devs!</h2>}
         <ActionSection>
-          <ButtonDarshboardPage type="button" onClick={() => handleSearchRecommendedUsers(!user ? { login: informedDevParameterToRecommendedDevs } : user)}>Recommended Search</ButtonDarshboardPage>
           { user &&
-            <ButtonDarshboardPage type="button" onClick={handleIsFollowingDev}>Follow info</ButtonDarshboardPage>
+            <>
+              <ButtonDarshboardPage type="button" onClick={handleIsFollowingDev}>Follow info</ButtonDarshboardPage>
+              <ButtonDarshboardPage type="button" onClick={() => handleSearchRecommendedUsers(!user ? { login: informedDevParameterToRecommendedDevs } : user)}>Recommended Search</ButtonDarshboardPage>
+            </>
           }
           { !user &&
             <>
@@ -469,13 +471,13 @@ export default function Home({ usersIds }: dashboardProps) {
               />
             </>
           }
-          {/* {!user &&
+          {!user &&
             <Link href={'/'} passHref={true}>
               <ButtonDarshboardPage type="button">
                 SignIn with your GitHub account
               </ButtonDarshboardPage>
             </Link>
-          } */}
+          }
         </ActionSection>
 
         <Footer />
